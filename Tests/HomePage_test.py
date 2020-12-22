13625a0f6841c8e5a06fc178373d93b89eef1814
main <o_galushko@yahoo.com>
2020-12-22 11:40:10 -0800

    my test

--- Tests/HomePage_test.py
+++ Tests/HomePage_test.py
@@ -1,8 +1,8 @@
 from Pages.HomePage import HomePage
 
-
+# for class Test should be the beginning
 class TestHomePage:
-
+# only test_ should be in the beginning
     def test_title(self):
         self.home_page = HomePage(self.driver)
         title = self.home_page.get_title()
