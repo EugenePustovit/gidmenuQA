5d6dbacef4e0bec08d23e911921cf4fd3e633250
ke-de-ve <kedeve@gmail.com>
2021-01-06 23:51:54 -0500

    changed page URL to use HOME_PAGE

--- Config/TestData.py
+++ Config/TestData.py
@@ -1,4 +1,4 @@
 class TestData:
 
-    HOME_PAGE_URL = 'http://www.gidmenu.com'
-    BREAKFASTS_PAGE_URL = 'https://gidmenu.com/restaurants/zavtraki/'
+    HOME_PAGE_URL = 'http://www.gidmenu.com';
+    BREAKFASTS_PAGE_URL = HOME_PAGE_URL + '/restaurants/zavtraki/';
