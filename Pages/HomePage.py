3cd2bac90a4243f599a58f11a2dc405a17146bbe
StanAretinskiy <sr575sr@gmail.com>
2021-01-06 20:06:17 -0800

    Save local changes 2 login test

--- Pages/HomePage.py
+++ Pages/HomePage.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from Config.TestData import TestData as TD
@@ -10,9 +9,8 @@ class HomePage(BasePage):
     TITLE = 'Гастрономический путеводитель'
 
     def __init__(self, driver):
-        super().__init__(driver)
+        super(HomePage, self).__init__(driver)
         self.page_url = TD.HOME_PAGE_URL
-        self.wait = WebDriverWait(self.driver, 10)
 
     def go_to_page(self):
         self.driver.get(self.page_url)
@@ -27,9 +25,7 @@ class HomePage(BasePage):
         self.driver.find_element(By.XPATH, "//button[text()='Войти']").click()
 
     def get_login_error_message(self):
-        modal_error_message_we = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#infoMsgModal div.modal-body")))
-        print("Element: ' {element}'".format(element=modal_error_message_we))
-        # print(type(modal_message_we))
-        error_message = modal_error_message_we.text
-        # print(error_message)
-        return error_message
+        modal_error_message = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#infoMsgModal div.modal-body"))).text
+        print("Error message get_login_error_message: ' {text}'".format(text=modal_error_message))
+        return self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#infoMsgModal div.modal-body")))
+    
--- Tests/Login_test.py
+++ Tests/Login_test.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from time import sleep
 from Pages.HomePage import HomePage
 
@@ -7,6 +10,7 @@ class TestLogin:
     def test_Login_invalid_password(self):
 
         self.home_page = HomePage(self.driver)
+        self.wait = WebDriverWait(self.driver, 10)
 
         self.home_page.go_to_page()
         self.home_page.click_profile_icon()
@@ -14,8 +18,11 @@ class TestLogin:
         self.home_page.enter_password("Hilton102")
         self.home_page.click_login()
         sleep(3)
+        modal_error_message_we = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#infoMsgModal div.modal-body")))
+        print("Element: {element}".format(element=modal_error_message_we))
+        error_message = self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div#infoMsgModal div.modal-body"))).text
+        print("Error message test_login: '{element}'".format(element=error_message))
         err_msg = self.home_page.get_login_error_message
-        print('Error message:')
-        print(err_msg)
+        print('Error message: {text}'.format(text=err_msg))
         sleep(3)
-        assert err_msg == 'Неверный логин или пароль.'
+        assert err_msg.text == 'Неверный логин или пароль.'
