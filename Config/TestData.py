a3ea24e342ee67a306f0faa38780c50971324b65
ke-de-ve <kedeve@gmail.com>
2021-01-07 08:35:25 -0500

    added RESTAURANT_COLLECTION

--- Config/TestData.py
+++ Config/TestData.py
@@ -1,4 +1,11 @@
 class TestData:
 
-    HOME_PAGE_URL = 'http://www.gidmenu.com';
-    BREAKFASTS_PAGE_URL = HOME_PAGE_URL + '/restaurants/zavtraki/';
+    HOME_PAGE_URL = 'http://www.gidmenu.com'
+    BREAKFASTS_PAGE_URL = HOME_PAGE_URL + '/restaurants/zavtraki/'
+
+    PATRIARSHIE_PRUDY = {
+        'url': 'https://gidmenu.com/restaurants/patriarshie-prudy/?show_dish_rest_groups=N',
+        'title': 'Патриаршие Пруды'
+    }
+
+    RESTAURANT_COLLECTION = [PATRIARSHIE_PRUDY]
