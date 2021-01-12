7b6525c7e3a5c7301671901ca051974bf2102395
ke-de-ve <kedeve@gmail.com>
2021-01-12 14:29:42 -0500

    implemented all recomendations from review

--- Tests/BreakfastsPage_test.py
+++ Tests/BreakfastsPage_test.py
@@ -1,8 +1,3 @@
-from time import sleep
-
-import pytest
-
-from Config.TestData import TestData
 from Pages.BreakfastPage import BreakfastPage
 
 
@@ -10,11 +5,9 @@ class TestBreakfastPage:
 
     HOME_BUTTON = '//header//a[@class="navbar-brand"]'
 
-#    @pytest.mark.parametrize('restaurant', TestData.RESTAURANT_COLLECTION)
     def test_title(self):
         self.breakfasts_page = BreakfastPage(self.driver)
         title = self.breakfasts_page.get_title()
 
         assert title == BreakfastPage.TITLE
-        sleep(5)
 
