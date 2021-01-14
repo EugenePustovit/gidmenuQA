e3966b3514f820a8a744f7c32c88a44f5b90ba87
maxim ashuev <maximashuev@gmail.com>
2021-01-13 22:39:27 -0600

    move @pytest.mark.parametrize under class

--- Pages/BasePage.py
+++ Pages/BasePage.py
@@ -1,8 +1,7 @@
 class BasePage:
 
-    def __init__(self, driver, timeout=10):
+    def __init__(self, driver):
         self.driver = driver
-        self.timeout =timeout
 
     def get_title(self):
         return self.driver.title
--- Tests/RestaurantsPage_test.py
+++ Tests/RestaurantsPage_test.py
@@ -3,9 +3,9 @@ from Config.TestData import TestData
 from Pages.RestaurantsPage import RestaurantsPage
 
 
+@pytest.mark.parametrize('restaurant', TestData.RESTAURANTS_COLLECTION, ids=TestData.LIST_OF_RESTAURANTS_NAMES)
 class TestRestaurantsPage:
 
-    @pytest.mark.parametrize('restaurant', TestData.RESTAURANTS_COLLECTION, ids=TestData.LIST_OF_RESTAURANTS_NAMES)
     def test_title(self, restaurant):
         self.restaurant = RestaurantsPage(self.driver, restaurant['url'])
         title = self.restaurant.get_title()
