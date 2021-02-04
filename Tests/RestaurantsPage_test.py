64dec015643a9ad499670f3a88df244d8388bacc
irinabarnes <irina.chabot@gmail.com>
2021-02-03 20:53:37 -0800

    assert fixes

--- Tests/RestaurantsPage_test.py
+++ Tests/RestaurantsPage_test.py
@@ -17,21 +17,23 @@ class TestRestaurantsPage:
     def test_logo_visible(self, restaurant):
         self.restaurant = RestaurantsPage(self.driver, restaurant['url'])
 
-        assert BasePage.is_visible(RestaurantsPage.LOGO)
+        assert self.restaurant.is_visible(RestaurantsPage.LOGO)
 
     def test_dish_group_is_visible(self, restaurant):
         self.restaurant = RestaurantsPage(self.driver, restaurant['url'])
 
-        assert BasePage.is_visible(RestaurantsPage.DISH_GROUP)
-        assert BasePage.is_visible(RestaurantsPage.RESTAURANT_IN_GROUP)
-        assert BasePage.is_visible(RestaurantsPage.ALCOHOL)
-        assert BasePage.is_visible(RestaurantsPage.DISH)
+        assert self.restaurant.is_visible(RestaurantsPage.DISH_GROUP)
+        assert self.restaurant.is_visible(RestaurantsPage.RESTAURANT_IN_GROUP)
+        assert self.restaurant.is_visible(RestaurantsPage.ALCOHOL)
+        assert self.restaurant.is_visible(RestaurantsPage.DISH)
 
     def test_dish_alcohol_is_clickable(self, restaurant):
         self.restaurant = RestaurantsPage(self.driver, restaurant['url'])
 
-        assert BasePage.is_clickable(RestaurantsPage.ALCOHOL)
-        assert BasePage.is_clickable(RestaurantsPage.DISH)
-        assert BasePage.is_clickable(RestaurantsPage.RESTAURANT_IN_GROUP)
+        assert self.restaurant.is_clickable(RestaurantsPage.ALCOHOL)
+        assert self.restaurant.is_clickable(RestaurantsPage.DISH)
+        assert self.restaurant.is_clickable(RestaurantsPage.RESTAURANT_IN_GROUP)
+
+
 
 
